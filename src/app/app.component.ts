e68d6d9d1de76cee08af2003c6709bdc1bfc5333
Rick Gibson <ricklesgibson@gmail.com>
2016-10-29 18:01:20 -0700

    Create recipes component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 
 import { HeaderComponent } from './header.component';
+import { RecipesComponent } from './recipes';
 
 @Component({
   selector: 'rb-root',
   templateUrl: './app.component.html',
-  directive: [HeaderComponent]
+  directive: [HeaderComponent, RecipesComponent]
 })
 export class AppComponent {
-  title = 'rb works!';
 }
